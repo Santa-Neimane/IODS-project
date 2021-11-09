3d62e2619335f287b3a8b298297efc54e78fefe6
Localadmin_neimane <Localadmin_neimane@LH0-570-23658.ad.helsinki.fi>
2021-11-09 11:04:38 +0200

    Data preliminary exploration

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -1,3 +1,15 @@
 # Author: Santa Neimane
 # Date: 09.11.2021
-# Regression and model validation tasks
+# Regression and model validation tasks /learning2014 data/ 
+
+data <- read.table(url("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt"), header = TRUE)
+str(data)
+head(data)
+# Data consists of 60 columns and 183 rows. 
+# Data shows points survey responses and includes participants age and gender info.
+
+data$gender <- as.factor(data$gender) #Set gender as a factor to see statistics
+summary(data)
+# Mean age of the participant is 26 years.  
+# Youngest was 17 years old, whilst oldest was 55 years old. 
+# Most (122) participants were female. 
